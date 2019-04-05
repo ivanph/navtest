cfc59c3de122d8acdef9b98fe1022678562b4539
Ivan Pintado <ivan.pintadoh@gmail.com>
2019-04-05 00:10:21 -0700

    Show hide nav bar

---
+++ app/src/main/java/com/test/navtest/Dashboard.kt
@@ -0,0 +1,38 @@
+package com.test.navtest
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_dashboard.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class Dashboard : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnNext.setOnClickListener { view.findNavController().navigate(R.id.next) }
+    }
+
+
+}
--- app/src/main/java/com/test/navtest/MainActivity.kt
+++ app/src/main/java/com/test/navtest/MainActivity.kt
@@ -1,34 +1,40 @@
 package com.test.navtest
 
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v7.app.AppCompatActivity
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        setupNavigation()
     }
+
+    private fun setupNavigation() {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(navigation, navController)
+        navController.addOnDestinationChangedListener { ctrl, destination, arguments ->
+            when(destination.id) {
+                R.id.navigation_dashboard -> setNavBarVisible(true)
+                R.id.navigation_home -> setNavBarVisible(true)
+                R.id.navigation_notifications -> setNavBarVisible(true)
+                else -> setNavBarVisible(false)
+            }
+
+        }
+    }
+
+    private fun setNavBarVisible(visible: Boolean) {
+        val state = if (visible) View.VISIBLE else View.GONE
+        navigation.visibility = state
+    }
+
+
 }
